a0932c4b036752a08eaf557c6abd225b3c9b3e10
Dylan Pion <dylanpion@mbp-de-dylan.home>
2023-09-09 16:47:50 +0200

    debut authentification

--- backend/src/main/java/com/WebCrawling/BackendApplication.java
+++ backend/src/main/java/com/WebCrawling/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.WebCrawling.controller;
+package com.WebCrawling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- frontend/src/Component/FormUrl.js
+++ frontend/src/Component/FormUrl.js
@@ -1,24 +1,33 @@
-// FormUrl.js
 import React, { useState } from "react";
 import axios from "axios";
 
 function FormUrl() {
-  // Etat local pour stocker l'URL entrée par l'utilisateur
   const [url, setUrl] = useState("");
+  const [isValidUrl, setIsValidUrl] = useState(true);
 
   const handleUrlChange = (e) => {
-    // Mise à jour de l'état 'url' avec la nouvelle valeur du champ de saisie
-    setUrl(e.target.value);
+    const inputUrl = e.target.value;
+    setUrl(inputUrl);
+    // Utiliser une regex pour vérifier si l'URL est valide
+    const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+    setIsValidUrl(urlRegex.test(inputUrl));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (!isValidUrl) {
+      console.error("URL invalide.");
+      return;
+    }
+
     try {
-      // Envoie des données du formulaire au serveur en utilisant Axios
-      const response = await axios.post("http://localhost:8080/scraped-data", {
+      await axios.post("http://localhost:8080/scraped-data", {
         url,
       });
+
+      // Une fois que la requête Axios est terminée avec succès, rechargez la page.
+      window.location.reload();
     } catch (error) {
       console.error("Erreur lors de l'envoi au serveur :", error);
     }
@@ -33,9 +42,11 @@ function FormUrl() {
           value={url}
           onChange={handleUrlChange}
           placeholder="Entrez l'URL ici"
+          style={{ borderColor: isValidUrl ? "initial" : "red" }}
         />
       </label>
       <br />
+      {!isValidUrl && <p style={{ color: "red" }}>URL invalide.</p>}
       <button type="submit">Envoyer</button>
     </form>
   );
